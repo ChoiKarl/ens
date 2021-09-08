bf8c75af02cdaaa9ade9765a2b9a395eb95bb656
ChoiKarl <253440030@qq.com>
2021-09-08 20:44:11 +0800

    本地读取db

--- lib/word_manager/word_manager.dart
+++ lib/word_manager/word_manager.dart
@@ -1,8 +1,13 @@
 import 'package:ens/word_manager/word.dart';
+import 'package:flutter/services.dart';
 import 'package:sqflite/sqflite.dart';
+import 'dart:io' as io;
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart';
 
 class WordManager {
   static WordManager? _instance;
+
   static WordManager get shared {
     _instance ??= WordManager();
     return _instance!;
@@ -16,8 +21,9 @@ class WordManager {
 
   Future<void> fetchWords() async {
     if (words.isNotEmpty) return;
+    String path = await loadLocalDB();
     if (_db == null) {
-      _db = await openDatabase('db/english.db');
+      _db = await openDatabase(path);
     }
 
     List<Map<String, dynamic>> result = await _db!.rawQuery('select * from word');
@@ -41,4 +47,19 @@ class WordManager {
 
     return;
   }
+}
+
+Future<String> loadLocalDB() async {
+  io.Directory applicationDirectory = await getApplicationDocumentsDirectory();
+
+  String dbPathEnglish = path.join(applicationDirectory.path, "english.db");
+
+  bool dbExistsEnglish = await io.File(dbPathEnglish).exists();
+
+  if (!dbExistsEnglish) {
+    ByteData data = await rootBundle.load(path.join("assets", "english.db"));
+    List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+    await io.File(dbPathEnglish).writeAsBytes(bytes, flush: true);
+  }
+  return Future.value(dbPathEnglish);
 }
